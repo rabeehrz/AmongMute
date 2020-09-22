4639449b0597f16bffd2cfb7159dea4525285f6d
Mohammed Rabeeh <mohammadrabeeh@gmail.com>
2020-09-22 19:09:41 +0530

    Add support for multiple voice channels

--- bot.js
+++ bot.js
@@ -3,26 +3,29 @@ const config = require('./config.json');
 
 const client = new Discord.Client();
 
+client.login(config.BOT_TOKEN);
+
 const prefix = '/';
 
 let servers = {};
 
-const getMuteState = (guildId, channelID) => {
+const getMuteState = (guildId, channelId) => {
   let guild = servers[guildId];
   if (guild) {
-    if (guild[channelID]) {
-      return guild[channelID];
+    if (guild[channelId]) {
+      return guild[channelId];
     }
+  } else {
+    servers[guildId] = {};
   }
-  servers[guildId] = {};
-  servers[guildId][channelID] = false;
+  servers[guildId][channelId] = false;
   return false;
 };
 
 client.on('message', (message) => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
-  if (message.channel.name !== 'mutebot') return;
+  if (message.channel.name !== 'amongmute') return;
 
   const guild = message.guild;
   const commandBody = message.content.slice(prefix.length);
@@ -38,7 +41,6 @@ client.on('message', (message) => {
     if (voiceChannel) {
       const muted = getMuteState(guild.id, voiceChannel.id);
       const channel = guild.channels.cache.get(voiceChannel.id);
-      console.log(channel);
 
       for (const [memberID, member] of channel.members) {
         member.voice.setMute(!muted);
@@ -47,7 +49,7 @@ client.on('message', (message) => {
       message.reply(
         `You ${muted ? 'un' : ''}muted everyone in ${channel.name}`,
       );
-      console.log(servers);
+
       servers[guild.id][voiceChannel.id] = !muted;
     } else {
       message.reply('You need to join a voice channel first!');
@@ -55,4 +57,18 @@ client.on('message', (message) => {
   } else return;
 });
 
-client.login(config.BOT_TOKEN);
+// Handle muting when getting in and out of server
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+  const newChannelId = newMember.channelID;
+  const oldChannelId = oldMember.channelID;
+  if (oldChannelId !== newChannelId) {
+    if (newChannelId) {
+      newMember.member.voice.setMute(false);
+      const guildId = newMember.guild.id;
+      const channelId = newChannelId;
+      if (servers[guildId] && servers[guildId][channelId] == true) {
+        newMember.member.voice.setMute(true);
+      }
+    }
+  }
+});
