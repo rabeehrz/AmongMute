207fccf48efe3556cb32ee3fcdcf97a76aac4728
Mohammed Rabeeh <mohammadrabeeh@gmail.com>
2020-09-22 14:55:43 +0530

    Add support for multiple servers

--- bot.js
+++ bot.js
@@ -5,15 +5,26 @@ const client = new Discord.Client();
 
 const prefix = '/';
 
-let muted = false;
+let servers = {};
+
+const getMuteState = (guildId, channelID) => {
+  let guild = servers[guildId];
+  if (guild) {
+    if (guild[channelID]) {
+      return guild[channelID];
+    }
+  }
+  servers[guildId] = {};
+  servers[guildId][channelID] = false;
+  return false;
+};
 
 client.on('message', (message) => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
   if (message.channel.name !== 'mutebot') return;
 
-  console.log(message);
-
+  const guild = message.guild;
   const commandBody = message.content.slice(prefix.length);
   const args = commandBody.split(' ');
   const command = args.shift().toLowerCase();
@@ -21,19 +32,23 @@ client.on('message', (message) => {
   if (command === 'pong') {
     const timeTaken = Date.now() - message.createdTimestamp;
     message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-  } else if (command === 'tmc') {
-    if (message.member.voice.channel) {
-      let channel = message.guild.channels.cache.get(
-        message.member.voice.channel.id,
-      );
+  } else if (command === 'tm') {
+    const voiceChannel = message.member.voice.channel;
+
+    if (voiceChannel) {
+      const muted = getMuteState(guild.id, voiceChannel.id);
+      const channel = guild.channels.cache.get(voiceChannel.id);
+      console.log(channel);
 
       for (const [memberID, member] of channel.members) {
         member.voice.setMute(!muted);
       }
+
       message.reply(
         `You ${muted ? 'un' : ''}muted everyone in ${channel.name}`,
       );
-      muted = !muted;
+      console.log(servers);
+      servers[guild.id][voiceChannel.id] = !muted;
     } else {
       message.reply('You need to join a voice channel first!');
     }
